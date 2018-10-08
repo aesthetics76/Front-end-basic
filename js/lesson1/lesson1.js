ff8836290320886766670388dbd336f5e9f673cd
王笑宇 <ouemisakai@wangxiaoyudeMacBook-Pro.local>
2018-10-08 17:50:36 +0800

    Lesson1 first commit

---
+++ js/lesson1/lesson1.js
@@ -0,0 +1,20 @@
+var a = 1;
+var b = 2;
+var l1_array = [1,2,3];
+var l1_boolean = true;
+var l1_string = "xiaoyu"
+var l1_object = {
+    name: 'xiaoyu',
+    age: 23
+}
+
+if (a < b) {
+    console.log("a小于b");
+} else if(a > b) {
+    console.log("a大于b");
+} else {
+    console.log("a等于b");
+}
+
+//定义一个function
+
---
+++ js/lesson1/practice2.js
@@ -0,0 +1,2 @@
+// 请利用循环遍历数组中的每个名字，并显示Hello, xxx!：
+var arr = ['Bart', 'Lisa', 'Adam'];
